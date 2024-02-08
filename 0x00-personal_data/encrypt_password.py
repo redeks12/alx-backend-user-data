8291075285d088fafef361e87d88d475607493b5
kumdan job <redeks123456@gmail.com>
2024-02-08 20:24:09 +0100

    pe

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-'''0x00. Personal data'''
+"""0x00. Personal data"""
+
+from bcrypt import checkpw, gensalt, hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """User passwords should NEVER be stored in plain text in a database."""
+    return hashpw(password.encode(), gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Implement an is_valid function that expects 2 arguments and returns a boolean."""
+    return checkpw(password.encode(), hashed_password)
