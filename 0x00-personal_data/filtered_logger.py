b04709ed69f5cf4edd3346674f8cbf6c23912f6d
kumdan job <redeks123456@gmail.com>
2024-02-08 10:24:41 +0100

    pe

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,3 +13,28 @@ def filter_datum(
             f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message
         )
     return message
+
+
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records using filter_datum"""
+        record.msg: str = filter_datum(
+            self.fields,
+            redaction=self.REDACTION,
+            message=record.msg,
+            separator=self.SEPARATOR,
+        )
+        return super(RedactingFormatter, self).format(record)
