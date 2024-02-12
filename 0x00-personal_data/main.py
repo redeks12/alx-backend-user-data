7ded778da57b6d7cb3005b26d362f121b3afd16b
kumdan job <redeks123456@gmail.com>
2024-02-12 21:19:39 +0100

    m

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -2,6 +2,8 @@ hash_password = __import__("encrypt_password").hash_password
 is_valid = __import__("encrypt_password").is_valid
 
 password = "MyAmazingPassw0rd"
+passwor = "MyAmazingPassw0rd"
 encrypted_password = hash_password(password)
 print(encrypted_password)
 print(is_valid(encrypted_password, password))
+print(is_valid(encrypted_password, passwor))
