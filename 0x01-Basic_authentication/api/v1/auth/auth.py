e18f8c05718db5e60a6f71de8e05cd564808e167
kumdan job <redeks123456@gmail.com>
2024-02-13 18:22:21 +0100

    m

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,12 +14,31 @@ class Auth:
         """require authentication
         return
             - False"""
-        return False
+
+        if not excluded_paths or not path:
+            return True
+        if path.endswith("/"):
+            path = path[:-1]
+        for idx, ex in enumerate(excluded_paths):
+            if ex.endswith("/"):
+                excluded_paths[idx] = ex[:-1]
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """require authorization header
         - return False"""
-        return None
+
+        if request is None:
+            return None
+
+        val = request.headers.get("Authorization", None)
+        if val:
+            return val
+        else:
+            return None
 
     def current_user(self, request=None) -> TypeVar("User"):
         """get current user
