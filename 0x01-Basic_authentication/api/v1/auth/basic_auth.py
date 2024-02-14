7502eed985a4fd3fae990e4341ccca9506fca707
kumdan job <redeks123456@gmail.com>
2024-02-14 21:43:14 +0100

    m

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,6 +14,7 @@ class BasicAuth(Auth):
     """Basic authentication implementation"""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """extract authorization header from authorization header"""
         if authorization_header is None or type(authorization_header) is not str:
             return None
 
@@ -25,6 +26,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
         self, base64_authorization_header: str
     ) -> str:
+        """Decode base64 authorization header"""
         if (
             base64_authorization_header is None
             or type(base64_authorization_header) is not str
@@ -41,6 +43,7 @@ class BasicAuth(Auth):
     def extract_user_credentials(
         self, decoded_base64_authorization_header: str
     ) -> Tuple[str, str]:
+        """Extract credentials from the authorization"""
         if (
             decoded_base64_authorization_header is None
             or type(decoded_base64_authorization_header) is not str
@@ -57,6 +60,7 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
     ) -> TypeVar("User"):
+        """Returns a User object from credentials"""
         if user_email is None or type(user_email) is not str:
             return None
         if user_pwd is None or type(user_pwd) is not str:
@@ -69,6 +73,7 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar("User"):
+        """get the current user from the database"""
         authHead = self.authorization_header(request)
         if not authHead:
             return
