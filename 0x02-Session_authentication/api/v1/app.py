11aac7f095c0d270dbf6b7f3159f84e37d988a05
kumdan job <redeks123456@gmail.com>
2024-02-14 23:11:18 +0100

    session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,11 +36,19 @@ def before_request() -> None:
         return
     if not auth.require_auth(
         request.path,
-        ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"],
+        [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ],
     ):
         return
 
-    if auth.authorization_header(request) is None:
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
         abort(401)
     cur = auth.current_user(request)
     if cur is None:
